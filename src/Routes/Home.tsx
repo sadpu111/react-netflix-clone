c5cb43e5953a31ddf701a0332d2958f7f34dd825
sadpu111 <dwme7100@gmail.com>
2022-12-01 16:57:28 +0900

    slider part 1

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,3 +1,5 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 import { useQueries, useQuery } from "react-query";
 import styled from "styled-components";
 import { getMovies, IGetMoviesResult } from "../api";
@@ -30,20 +32,66 @@ const Overview = styled.p`
   font-size: 20px;
   width: 50%;
 `;
-
+const Slider = styled(motion.div)`
+  position: relative;
+  top: -100px;
+`;
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute; 
+  width: 100%;
+`;
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+  color: red;
+  font-size: 66px;
+`;
+const rowVariants = {
+  hidden: {
+    x: window.innerWidth + 10,  // 사용자 윈도우 너비
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.innerWidth -10, // -10은 1과 6이 붙어있는 것을 막기 위해
+  },
+};
 
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(["movies", "nowPlayng"], getMovies);
+  const [index, setIndex] = useState(0);
+  const increseIndex = () => setIndex((prev) => prev + 1);
   return (
     <Wraper>
       {isLoading ?
         <Loader>now loading...</Loader> :
         <>
-          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+          <Banner
+            onClick={increseIndex}
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
             {/* fallback(|| "")를 추가하여, 어떤 이유로 data가 정의되지 않을 때 ""를 출력하도록 한다 */}
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence>
+              <Row
+                key={index}
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{type: "tween", duration: 0.7}} // linear transition
+                /* increaseIndex로 key의 index 값이 증가할 때마다, react.js는 새로운 row가 추가되는 것으로 인식하고, 이에 따라 기존의 row는 exit로 사라진다(AnimationPresence를 통해) */
+                >
+                  {[1,2,3,4,5,6].map((i) => <Box key={i}>{i}</Box>)}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       }
     </Wraper>
