f36e4814deade11fe93d742fc23bdd17cc795696
sadpu111 <dwme7100@gmail.com>
2022-12-08 16:39:12 +0900

    add sliderBtn

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -5,9 +5,11 @@ import { useMatch, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { getMovies, IGetMoviesResult } from "../api";
 import { makeImagePath } from "../utils";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const Wraper = styled.div`
-overflow: hidden;
+  overflow: hidden;
   background-color: black;
   padding-bottom: 200px;
 `;
@@ -36,17 +38,39 @@ const Overview = styled.p`
   width: 50%;
 `;
 const Slider = styled(motion.div)`
+  column-gap: 5px;
   position: relative;
   top: -100px;
+  align-content: center;
 `;
-const Row = styled(motion.div)`
+const SliderBtn = styled.button<{ isRight: boolean }>`
+  position: absolute;
+  right: ${(props) => (props.isRight ? 0 : null)};
+  left: ${(props) => (props.isRight ? null : 0)};
+  background-color: rgba(0, 0, 0, 0.5);
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 40px;
+  border: none;
+  z-index: 2;
+  color: ${(props) => props.theme.white.darker};
+  svg {
+    width: 40px;
+    height: 40px;
+  }
+`;
+const Thumbnails = styled(motion.div)`
   display: grid;
-  gap: 5px;
+  column-gap: 10px;
   grid-template-columns: repeat(6, 1fr);
-  position: absolute; 
   width: 100%;
+  position: absolute;
 `;
-const Box = styled(motion.div) <{ bgPhoto: string }>`
+
+const Thumbnail = styled(motion.div) <{ bgPhoto: string }>`
   background-color: white;
   background-image: url(${(props) => props.bgPhoto});
   background-size: cover;
@@ -113,7 +137,7 @@ const Overlay = styled(motion.div)`
   background-color: rgba(0, 0, 0, 1);
   opacity: 0;
 `;
-const boxVariants = {
+const thumbnailVariants = {
   normal: {
     scale: 1,
   },
@@ -127,7 +151,7 @@ const boxVariants = {
     },
   },
 };
-const rowVariants = {
+const thumbnailsVariants = {
   hidden: {
     x: window.innerWidth + 5,  // 사용자 윈도우 너비
   },
@@ -176,7 +200,13 @@ function Home() {
     }
   };
   const decreaseIndex = () => {
-
+    if (data) {
+      if (leaving) return;
+      toggleLeaving(); // 수정 필요
+      const totalMovies = data?.results.length - 1;
+      const maxIndex = Math.floor(totalMovies / offset) - 1;
+      setIndex((prev) => prev === maxIndex ? 0 : prev - 1);
+    }
   };
   return (
     <Wraper>
@@ -184,7 +214,6 @@ function Home() {
         <Loader>now loading...</Loader> :
         <>
           <Banner
-            onClick={increseIndex}
             bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
             {/* fallback(|| "")를 추가하여, 어떤 이유로 data가 정의되지 않을 때 ""를 출력하도록 한다 */}
             <Title>{data?.results[0].title}</Title>
@@ -194,10 +223,9 @@ function Home() {
             <AnimatePresence onExitComplete={toggleLeaving} initial={false}>
               {/* onExitComplete => exit이 완료되면 실행되는 함수
             initial={false} => 화면 새로고침 시 애니메이션이 작동하지 않는다 */}
-            <button>왼쪽</button>
-              <Row
+              <Thumbnails
                 key={index}
-                variants={rowVariants}
+                variants={thumbnailsVariants}
                 initial="hidden"
                 animate="visible"
                 exit="exit"
@@ -206,28 +234,42 @@ function Home() {
               >
                 {data?.results.slice(1) // 배너 영화 1개를 제외한 영화 목록
                   .slice(offset * index, offset * index + offset).map((movie) => (
-                    <Box
+                    <Thumbnail
                       layoutId={movie.id + ""}
                       onClick={() => onBoxClicked(movie.id)}
                       /* onBoxClicked에 movie.id라는 전달인자를 넘겨주기 위해 () => 익명함수를 사용 */
-                      variants={boxVariants}
+                      variants={thumbnailVariants}
                       initial="normal"
                       whileHover="hover"
                       key={movie.id}
                       bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
                     >
-                      <Info
-                        variants={infoVariants}
-                      >
-                        <h4>
-                          {movie.title}
-                        </h4>
+                      <Info variants={infoVariants}>
+                        <h4>{movie.title}</h4>
                       </Info>
-                    </Box>
+                    </Thumbnail>
                   ))}
-              </Row>
-              <button>왼쪽</button>
+              </Thumbnails>
             </AnimatePresence>
+            <SliderBtn onClick={decreaseIndex} isRight={false}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 256 512"
+                fill="currentColor"
+              >
+                <path d="M137.4 406.6l-128-127.1C3.125 272.4 0 264.2 0 255.1s3.125-16.38 9.375-22.63l128-127.1c9.156-9.156 22.91-11.9 34.88-6.943S192 115.1 192 128v255.1c0 12.94-7.781 24.62-19.75 29.58S146.5 415.8 137.4 406.6z" />
+              </svg>
+            </SliderBtn>
+            <SliderBtn onClick={increseIndex}
+              isRight={true}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 256 512"
+                fill="currentColor"
+              >
+                <path d="M118.6 105.4l128 127.1C252.9 239.6 256 247.8 256 255.1s-3.125 16.38-9.375 22.63l-128 127.1c-9.156 9.156-22.91 11.9-34.88 6.943S64 396.9 64 383.1V128c0-12.94 7.781-24.62 19.75-29.58S109.5 96.23 118.6 105.4z" />
+              </svg>
+            </SliderBtn>
           </Slider>
           <AnimatePresence>
             {bigMovieMatch ? (
