ac1e9b3b4a86e491adf7505447a0c5b4bb05a24e
sadpu111 <dwme7100@gmail.com>
2022-12-08 19:47:23 +0900

    thumbnail & slideBtn

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -5,8 +5,7 @@ import { useMatch, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { getMovies, IGetMoviesResult } from "../api";
 import { makeImagePath } from "../utils";
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 
 const Wraper = styled.div`
   overflow: hidden;
@@ -45,8 +44,6 @@ const Slider = styled(motion.div)`
   top: -100px;
   align-content: center;
 `;
-const SliderBtns = styled.div`
-`;
 const SliderBtn = styled(motion.button) <{ isRight: boolean }>`
   position: absolute;
   right: ${(props) => (props.isRight ? 0 : null)};
@@ -72,33 +69,35 @@ const Thumbnails = styled(motion.div)`
   width: 100%;
   position: absolute;
 `;
-
 const Thumbnail = styled(motion.div) <{ bgPhoto: string }>`
-  background-color: white;
+  height: 150px;
+  font-size: 64px;
   background-image: url(${(props) => props.bgPhoto});
   background-size: cover;
   background-position: center center;
-  height: 150px;
-  font-size: 66px;
-  cursor: pointer;
+  justify-content: center;
+  align-items: center;
   &:first-child {
-    transform-origin: left;
+    transform-origin: center left;
   }
   &:last-child {
-    transform-origin: right;
+    transform-origin: center right;
   }
-  `;
-const Info = styled(motion.div)`
-  padding: 10px;
-  background-color: ${(props) => props.theme.black.lighter};
-  opacity: 0;
-  position: absolute;
-  width: 100%;
-  bottom: 0;
-  h4 {
-    text-align: center;
-    font-size: 18px;
-  }
+  cursor: pointer;
+`;
+export const Info = styled(motion.div)`
+padding: 10px;
+background-color: ${(props) => props.theme.black.darker};
+opacity: 0;
+position: absolute;
+width: 100%;
+bottom: 0;
+h4 {
+  color: white;
+  text-align: center;
+  font-size: 15px;
+  font-weight: 600;
+}
 `;
 const BigMovie = styled(motion.div)`
   position: absolute;
@@ -155,38 +154,49 @@ const sliderBtnVariants = {
 const thumbnailVariants = {
   normal: {
     scale: 1,
+    
   },
   hover: {
     scale: 1.3,
-    y: -50,
+    y: -30,
     transition: {
       delay: 0.5,
       duration: 0.3,
-      type: "tween"
+      type: "tween",
     },
   },
 };
 const thumbnailsVariants = {
-  hidden: {
-    x: window.innerWidth + 5,  // 사용자 윈도우 너비
-  },
+  hidden: ({ width, toPrev }:
+    {
+      width: number;
+      toPrev: boolean;
+    }) => ({
+      x: toPrev ? -width - 5 : width + 5  // 사용자 윈도우 너비
+    }),
   visible: {
     x: 0,
   },
-  exit: {
-    x: -window.innerWidth - 5, // -10은 1과 6이 붙어있는 것을 막기 위해
-  },
+  exit: ({ width, toPrev }:
+    {
+      width: number;
+      toPrev: boolean;
+    }) => ({
+      x: toPrev ? width + 5 : -width - 5 // 사용자 윈도우 너비
+    }),
 };
-const infoVariants = {
+export const infoVariants = {
   hover: {
-    opacity: 1,
     transition: {
       delay: 0.5,
-      duaration: 0.1,
+      duration: 0.3,
       type: "tween",
     },
+    opacity: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
-}
+};
+
 const offset = 6;
 
 function Home() {
@@ -196,6 +206,7 @@ function Home() {
   const onOverlayClick = () => {
     navigate(-1);
   };
+  const [toPrev, setToPrev] = useState(false);
   const { data, isLoading } = useQuery<IGetMoviesResult>(["movies", "nowPlayng"], getMovies);
   const clickedMovie = bigMovieMatch?.params.movieId && data?.results.find((movie) => movie.id + "" === bigMovieMatch?.params.movieId);
   const [index, setIndex] = useState(0);
@@ -203,24 +214,30 @@ function Home() {
   const toggleLeaving = () => setLeaving((prev) => !prev);
   const onBoxClicked = (movieId: number) => {
     navigate(`/movies/${movieId}`);
-  }
+  };
+  const width = window.innerWidth;
   const increseIndex = () => {
     if (data) {
       if (leaving) return;
+      setToPrev(false);
       toggleLeaving(); // setLeaving(true)는 항상 true가 되서 다른 동작이 되지 않음
       const totalMovies = data?.results.length - 1; // if (data)를 통해 maybe undefined 오류 방지. 배너 영화 한 개를 제외한 총 개수
       const maxIndex = Math.floor(totalMovies / offset) - 1; // 1개 row당 보여지는 영화의 개수(offset=6)으로 나눠 index 상한 설정
       setIndex((prev) => prev === maxIndex ? 0 : prev + 1);
-    }
+    };
+    console.log(index, toPrev)
   };
   const decreaseIndex = () => {
     if (data) {
       if (leaving) return;
-      toggleLeaving(); // 수정 필요
+      setToPrev(true);
+      toggleLeaving();
       const totalMovies = data?.results.length - 1;
       const maxIndex = Math.floor(totalMovies / offset) - 1;
-      setIndex((prev) => prev === maxIndex ? 0 : prev - 1);
-    }
+      const minIndex = 0;
+      setIndex((prev) => prev === minIndex ? maxIndex : prev - 1);
+    };
+    console.log(index, toPrev)
   };
   return (
     <Wraper>
@@ -234,10 +251,11 @@ function Home() {
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
           <Slider>
-            <AnimatePresence onExitComplete={toggleLeaving} initial={false}>
+            <AnimatePresence onExitComplete={toggleLeaving} initial={false} custom={{ width, toPrev }}>
               {/* onExitComplete => exit이 완료되면 실행되는 함수
             initial={false} => 화면 새로고침 시 애니메이션이 작동하지 않는다 */}
               <Thumbnails
+                custom={{ width, toPrev }}
                 key={index}
                 variants={thumbnailsVariants}
                 initial="hidden"
@@ -257,6 +275,7 @@ function Home() {
                       whileHover="hover"
                       key={movie.id}
                       bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                      transition={{type: "tween"}}
                     >
                       <Info variants={infoVariants}>
                         <h4>{movie.title}</h4>
@@ -265,36 +284,34 @@ function Home() {
                   ))}
               </Thumbnails>
             </AnimatePresence>
-            <SliderBtns >
-              <SliderBtn
-                onClick={decreaseIndex} 
-                isRight={false} 
-                variants={sliderBtnVariants} 
-                initial="normal" 
-                whileHover="hover">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 256 512"
-                  fill="currentColor"
-                >
-                  <path d="M137.4 406.6l-128-127.1C3.125 272.4 0 264.2 0 255.1s3.125-16.38 9.375-22.63l128-127.1c9.156-9.156 22.91-11.9 34.88-6.943S192 115.1 192 128v255.1c0 12.94-7.781 24.62-19.75 29.58S146.5 415.8 137.4 406.6z" />
-                </svg>
-              </SliderBtn>
-              <SliderBtn 
-                onClick={increseIndex} 
-                isRight={true}
-                variants={sliderBtnVariants} 
-                initial="normal" 
-                whileHover="hover">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 256 512"
-                  fill="currentColor"
-                >
-                  <path d="M118.6 105.4l128 127.1C252.9 239.6 256 247.8 256 255.1s-3.125 16.38-9.375 22.63l-128 127.1c-9.156 9.156-22.91 11.9-34.88 6.943S64 396.9 64 383.1V128c0-12.94 7.781-24.62 19.75-29.58S109.5 96.23 118.6 105.4z" />
-                </svg>
-              </SliderBtn>
-            </SliderBtns>
+            <SliderBtn
+              onClick={decreaseIndex}
+              isRight={false}
+              variants={sliderBtnVariants}
+              initial="normal"
+              whileHover="hover">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 256 512"
+                fill="currentColor"
+              >
+                <path d="M137.4 406.6l-128-127.1C3.125 272.4 0 264.2 0 255.1s3.125-16.38 9.375-22.63l128-127.1c9.156-9.156 22.91-11.9 34.88-6.943S192 115.1 192 128v255.1c0 12.94-7.781 24.62-19.75 29.58S146.5 415.8 137.4 406.6z" />
+              </svg>
+            </SliderBtn>
+            <SliderBtn
+              onClick={increseIndex}
+              isRight={true}
+              variants={sliderBtnVariants}
+              initial="normal"
+              whileHover="hover">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 256 512"
+                fill="currentColor"
+              >
+                <path d="M118.6 105.4l128 127.1C252.9 239.6 256 247.8 256 255.1s-3.125 16.38-9.375 22.63l-128 127.1c-9.156 9.156-22.91 11.9-34.88 6.943S64 396.9 64 383.1V128c0-12.94 7.781-24.62 19.75-29.58S109.5 96.23 118.6 105.4z" />
+              </svg>
+            </SliderBtn>
           </Slider>
           <AnimatePresence>
             {bigMovieMatch ? (
