cd67b48feb8029bd09d17cd975e943a0025d2c52
sadpu111 <dwme7100@gmail.com>
2022-12-08 17:34:24 +0900

    button desing

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -12,6 +12,7 @@ const Wraper = styled.div`
   overflow: hidden;
   background-color: black;
   padding-bottom: 200px;
+  align-items: center;
 `;
 const Loader = styled.div`
   display: flex;
@@ -38,27 +39,29 @@ const Overview = styled.p`
   width: 50%;
 `;
 const Slider = styled(motion.div)`
+  height: 150px;
   column-gap: 5px;
   position: relative;
   top: -100px;
   align-content: center;
 `;
-const SliderBtn = styled.button<{ isRight: boolean }>`
+const SliderBtns = styled.div`
+`;
+const SliderBtn = styled(motion.button) <{ isRight: boolean }>`
   position: absolute;
   right: ${(props) => (props.isRight ? 0 : null)};
   left: ${(props) => (props.isRight ? null : 0)};
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgba(0,0,0,0.5);
   cursor: pointer;
   display: flex;
   align-items: center;
   justify-content: center;
   height: 100%;
-  width: 40px;
+  width: 35px;
   border: none;
-  z-index: 2;
-  color: ${(props) => props.theme.white.darker};
+  color: ${(props) => props.theme.white.lighter};
   svg {
-    width: 40px;
+    width: 28px;
     height: 40px;
   }
 `;
@@ -137,6 +140,18 @@ const Overlay = styled(motion.div)`
   background-color: rgba(0, 0, 0, 1);
   opacity: 0;
 `;
+const sliderBtnVariants = {
+  normal: {
+    opacity: 0,
+  },
+  hover: {
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+      type: "tween"
+    },
+  },
+};
 const thumbnailVariants = {
   normal: {
     scale: 1,
@@ -183,7 +198,6 @@ function Home() {
   };
   const { data, isLoading } = useQuery<IGetMoviesResult>(["movies", "nowPlayng"], getMovies);
   const clickedMovie = bigMovieMatch?.params.movieId && data?.results.find((movie) => movie.id + "" === bigMovieMatch?.params.movieId);
-  console.log(clickedMovie);
   const [index, setIndex] = useState(0);
   const [leaving, setLeaving] = useState(false);
   const toggleLeaving = () => setLeaving((prev) => !prev);
@@ -251,25 +265,36 @@ function Home() {
                   ))}
               </Thumbnails>
             </AnimatePresence>
-            <SliderBtn onClick={decreaseIndex} isRight={false}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 256 512"
-                fill="currentColor"
-              >
-                <path d="M137.4 406.6l-128-127.1C3.125 272.4 0 264.2 0 255.1s3.125-16.38 9.375-22.63l128-127.1c9.156-9.156 22.91-11.9 34.88-6.943S192 115.1 192 128v255.1c0 12.94-7.781 24.62-19.75 29.58S146.5 415.8 137.4 406.6z" />
-              </svg>
-            </SliderBtn>
-            <SliderBtn onClick={increseIndex}
-              isRight={true}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 256 512"
-                fill="currentColor"
-              >
-                <path d="M118.6 105.4l128 127.1C252.9 239.6 256 247.8 256 255.1s-3.125 16.38-9.375 22.63l-128 127.1c-9.156 9.156-22.91 11.9-34.88 6.943S64 396.9 64 383.1V128c0-12.94 7.781-24.62 19.75-29.58S109.5 96.23 118.6 105.4z" />
-              </svg>
-            </SliderBtn>
+            <SliderBtns >
+              <SliderBtn
+                onClick={decreaseIndex} 
+                isRight={false} 
+                variants={sliderBtnVariants} 
+                initial="normal" 
+                whileHover="hover">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 256 512"
+                  fill="currentColor"
+                >
+                  <path d="M137.4 406.6l-128-127.1C3.125 272.4 0 264.2 0 255.1s3.125-16.38 9.375-22.63l128-127.1c9.156-9.156 22.91-11.9 34.88-6.943S192 115.1 192 128v255.1c0 12.94-7.781 24.62-19.75 29.58S146.5 415.8 137.4 406.6z" />
+                </svg>
+              </SliderBtn>
+              <SliderBtn 
+                onClick={increseIndex} 
+                isRight={true}
+                variants={sliderBtnVariants} 
+                initial="normal" 
+                whileHover="hover">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 256 512"
+                  fill="currentColor"
+                >
+                  <path d="M118.6 105.4l128 127.1C252.9 239.6 256 247.8 256 255.1s-3.125 16.38-9.375 22.63l-128 127.1c-9.156 9.156-22.91 11.9-34.88 6.943S64 396.9 64 383.1V128c0-12.94 7.781-24.62 19.75-29.58S109.5 96.23 118.6 105.4z" />
+                </svg>
+              </SliderBtn>
+            </SliderBtns>
           </Slider>
           <AnimatePresence>
             {bigMovieMatch ? (
